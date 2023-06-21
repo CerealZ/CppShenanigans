aa23f0a8c3a503c4163a8a4445ff89ca1efebc0b
Pierre Bienert <cerealz1109@googlemail.com>
2023-06-21 22:35:31 +0000

    Create signal.hpp

---
+++ dreambeard/signal.hpp
@@ -0,0 +1,66 @@
+#pragma once
+#include <functional>
+#include <iostream>
+
+template <typename  T>
+class Signal {
+	using on_change = std::function<void(Signal&)>;
+	T _value;
+	on_change _on_change;
+
+public:
+	explicit Signal(const on_change& callback) : _on_change(callback) { }
+	Signal(T value, on_change callback) : _value(value), _on_change(std::move(callback)) { }
+
+	/*explicit Signal(const on_change callback) : _on_change(callback) { }
+	Signal(T value, const on_change callback) : _on_change(callback), _value(value) { }*/
+
+	Signal& emit() {
+		this->_on_change(*this);
+		return *this;
+	}
+
+	const Signal& operator -- (T) {
+		--this->_value;
+		return this->emit();
+	}
+	const Signal& operator ++ (T) {
+		++this->_value;
+		return this->emit();
+	}
+
+	const Signal& operator += (const T& right) {
+		this->_value += right;
+		return this->emit();
+	}
+	const Signal& operator -= (const T& right) {
+		this->_value -= right;
+		return this->emit();
+	}
+
+	const Signal& operator *= (const T& right) const {
+		this->_value *= right;
+		return this->emit();
+	}
+	const Signal& operator /= (const T& right) const {
+		this->_value /= right;
+		return this->emit();
+	}
+
+	bool operator < (const T& right) const { return this->_value < right; }
+	bool operator <= (const T& right) const { return this->_value <= right; }
+	bool operator > (const T& right) const { return this->_value > right; }
+	bool operator >= (const T& right) const { return this->_value >= right; }
+	bool operator == (const T& right) const { return this->_value == right; }
+
+	bool operator < (const Signal& right) const { return this->_value < right._value; }
+	bool operator <= (const Signal& right) const { return this->_value <= right._value; }
+	bool operator > (const Signal& right) const { return this->_value > right._value; }
+	bool operator >= (const Signal& right) const { return this->_value >= right._value; }
+	bool operator == (const Signal& right) const { return this->_value == right._value; }
+
+	friend std::ostream& operator << (std::ostream& os, const Signal& self) {
+		os << self._value;
+		return os;
+	}
+};
